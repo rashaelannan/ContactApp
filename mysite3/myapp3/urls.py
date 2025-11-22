a46d43923c091ebf666552ce0b073b2094278742
rashaelannan <207150879+rashaelannan@users.noreply.github.com>
2025-11-23 01:12:34 +0200

    Remove duplicate 'recommend' path from URLs

--- mysite3/myapp3/urls.py
+++ mysite3/myapp3/urls.py
@@ -1,10 +1,12 @@
+
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.contact_list, name='contact_list'),
+    path('recommend/', views.recommend_contacts, name='recommend_contacts'),
     path('contacts/new/', views.contact_create, name='contact_create'),
     path('contacts/<int:pk>/edit/', views.contact_update, name='contact_update'),
     path('contacts/<int:pk>/delete/', views.contact_delete, name='contact_delete'),
-    path('recommend/', views.recommend_contacts, name='recommend_contacts'),
 ]
+
